c6b82357af6da790d3d0ff19b2581cb4b5731b2f
Michał Fereniec <michalkoks99@gmail.com>
2024-02-18 18:10:15 +0100

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Michal Fereniec on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
